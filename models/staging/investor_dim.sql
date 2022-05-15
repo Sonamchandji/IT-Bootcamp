1a3687f35ed8067a5ca97b3d032d403fe01f4dd7
pratibhachowdary@gmail.com <pratibhachowdary@gmail.com>
2022-05-15 12:38:47 +0000

    Attempting to install package to dbt

--- models/staging/investor_dim.sql
+++ models/staging/investor_dim.sql
@@ -1,5 +1,6 @@
 with investor as (
     select
+        {{ dbt_utils.surrogate_key(['investor_name', 'investor_types', 'date']) }} as investor_id,
         investor_name,
         investor_types
     from {{ref('pe_dataset')}})
