034dc64c4cbcf4d7ae87b928048a72d5382deb0e
pratibhachowdary@gmail.com <pratibhachowdary@gmail.com>
2022-05-16 23:35:31 +0000

    New inner join syntax attempted

--- models/staging/staging_pe.sql
+++ models/staging/staging_pe.sql
@@ -1,13 +1,18 @@
-With table_1 as (
-Select 
-date
-,date_id
-From {{ref('date_dim')}}
-),
-pe_hash_dataset as (
-    select * from {{ref('pe_hash_dataset')}}
-)
-Select
-pe_hash_dataset 
-,table_1
-From pe_hash_dataset Inner join table_1 on pe_hash_dataset.date = table_1.date
+--With table_1 as (
+--Select 
+--date
+--,date_id
+--From {{ref('date_dim')}}
+--),
+--pe_hash_dataset as (
+--    select * from {{ref('pe_hash_dataset')}}
+--)
+--Select
+--pe_hash_dataset 
+--,table_1
+--From pe_hash_dataset Inner join table_1 on pe_hash_dataset.date = table_1.date
+
+select *
+from {{ref('pe_hash_dataset')}} 
+inner join {{ref('date_dim')}} 
+on {{ref('pe_hash_dataset')}}.date = {{ref('date_dim')}}.date 
