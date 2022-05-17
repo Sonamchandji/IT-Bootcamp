0e96059971aa4d128cd7c027fbfb64e65c1970cc
pratibhachowdary@gmail.com <pratibhachowdary@gmail.com>
2022-05-17 08:08:42 +0000

    Successfullly created join table

--- models/work/wrk_join_pe.sql
+++ models/work/wrk_join_pe.sql
@@ -1,35 +1,6 @@
 {{config (materialized = "view")}}
--- with dt_temp as ( 
-  --select
-     --date
-    --,date_id
-  --from {{ref('stg_date')}}
---)
 
-select 
-company_name
-    ,growth_stage
-    ,country
-    ,state
-    ,city
-    ,continent
-    ,industry
-    ,sub_industry
-    ,client__focus
-    ,business_model
-    ,company_status
-    ,round
-    ,amount_raised
-    ,currency
-    ,pe.date
-    ,investor_types
-    ,investor_name
-    ,company_valuation
-    ,valuation_date
-    ,investor_id
-    ,company_id
-    ,country_id
-from {{ref('wrk_hash_pe')}} as pe
-join {{ref('stg_date')}} as dt
-on pe.date = dt.date
+
+select pe.*, dt.date_id
+from {{ref('wrk_hash_pe')}} as pe, {{ref('stg_date')}} as dt
 
